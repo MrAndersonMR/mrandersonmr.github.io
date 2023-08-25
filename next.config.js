4592625b5aa670eb12550dbfd144232b727da110
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2023-08-24 23:07:42 -0300

    to make it pass in github pages

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 }
 
 module.exports = nextConfig
